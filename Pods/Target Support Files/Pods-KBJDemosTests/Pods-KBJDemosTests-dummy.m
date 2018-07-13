dd5539e77dac46dd1dd4347c7f6efbc81d0b6948
JusterKinbo <justerkinbo@163.com>
2018-07-13 20:27:34 +0800

    删除pods

--- Pods/Target Support Files/Pods-KBJDemosTests/Pods-KBJDemosTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_KBJDemosTests : NSObject
-@end
-@implementation PodsDummy_Pods_KBJDemosTests
-@end
--- Pods/Target Support Files/Pods-KBJDemosUITests/Pods-KBJDemosUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_KBJDemosUITests : NSObject
-@end
-@implementation PodsDummy_Pods_KBJDemosUITests
-@end
